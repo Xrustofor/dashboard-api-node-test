fded1379cfa3e97eeb261f9d7a2457c0ada7996b
XrustoforK <oleksandr.Kyryliuk@bureau.kyiv.ua>
2022-06-13 15:45:38 +0300

    class

--- test.ts
+++ test.ts
@@ -1,20 +1,90 @@
-interface HasLength {
-    length: number
+class Coord {
+    message = "1";
+    lat: number;
+    long: number;
+
+    protected test() {
+
+    };
+
+    computeDistans(newLat: number, newLong: number){
+        return 0;
+    };
+
+    constructor(lat: number, long: number){
+        this.lat = lat;
+        this.long = long;
+    }
 }
 
-function log<T extends HasLength, K>(obj: T,  arr: K[]): K[]{
-   console.log(obj.length);
-   return arr;
+const point = new Coord(0, 1);
+
+class MapLocation extends Coord{
+    message = "2";
+    _name: string;
+
+    get name() {
+        return this._name;
+    };
+
+    set name(s: string) {
+        this._name = s;
+    };
+
+    override computeDistans(newLat: number, newLong: number){
+        console.log(this._name);
+        return 1;
+    };
+
+
+    constructor(lat: number, long: number, name: string){
+        super(lat, long);
+    }
 }
 
-log<string, number>('asdas', [1]);
+interface loggerService {
+    log: (s: string) => void;
+}
 
-interface IUser {
+class Logger implements loggerService{
+    public log(s: string){
+        console.log(s);
+    }
+    private error() {
+
+    }
+}
+
+const l = new Logger();
+
+
+class myClass<T> {
+    a: T;
+}
+
+const b = new myClass<string>();
+b.a
+
+abstract class Base{
+    print(s: string){
+        console.log(s)
+    }
+    abstract error (s: string): void;
+}
+
+class BaseExtend extends Base{
+
+}
+
+new BaseExtend().error
+
+class Animal {
     name: string;
-    age?: number;
-    bi1d: <T>(sum: T) => boolean
+}
+class Dog {
+    name: string;
+    tail: boolean;
 }
 
-function bid<T>(sum: T): boolean {
-    return true;
-}
+const puppy: Animal = new Dog();
+puppy.name;
