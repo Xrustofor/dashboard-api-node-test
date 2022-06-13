25258790e6e4f36b061d9924da6a206769bd8cd7
XrustoforK <oleksandr.Kyryliuk@bureau.kyiv.ua>
2022-06-13 15:50:17 +0300

    typeof & keyof

--- test.ts
+++ test.ts
@@ -1,90 +1,17 @@
-class Coord {
-    message = "1";
+let a = 'Привет'
+if(typeof a == 'string'){
+
+}
+
+let b: typeof a;
+
+
+
+type Coord = {
     lat: number;
     long: number;
-
-    protected test() {
-
-    };
-
-    computeDistans(newLat: number, newLong: number){
-        return 0;
-    };
-
-    constructor(lat: number, long: number){
-        this.lat = lat;
-        this.long = long;
-    }
 }
 
-const point = new Coord(0, 1);
+type P = keyof Coord;
 
-class MapLocation extends Coord{
-    message = "2";
-    _name: string;
-
-    get name() {
-        return this._name;
-    };
-
-    set name(s: string) {
-        this._name = s;
-    };
-
-    override computeDistans(newLat: number, newLong: number){
-        console.log(this._name);
-        return 1;
-    };
-
-
-    constructor(lat: number, long: number, name: string){
-        super(lat, long);
-    }
-}
-
-interface loggerService {
-    log: (s: string) => void;
-}
-
-class Logger implements loggerService{
-    public log(s: string){
-        console.log(s);
-    }
-    private error() {
-
-    }
-}
-
-const l = new Logger();
-
-
-class myClass<T> {
-    a: T;
-}
-
-const b = new myClass<string>();
-b.a
-
-abstract class Base{
-    print(s: string){
-        console.log(s)
-    }
-    abstract error (s: string): void;
-}
-
-class BaseExtend extends Base{
-
-}
-
-new BaseExtend().error
-
-class Animal {
-    name: string;
-}
-class Dog {
-    name: string;
-    tail: boolean;
-}
-
-const puppy: Animal = new Dog();
-puppy.name;
+let f: P = 'long'
